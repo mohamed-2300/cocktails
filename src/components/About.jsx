c95da9777a1228150d47bae8a9c0e6f2e0b419d2
mohamed-2300 <mohamedaitbourice@gmail.com>
2025-06-30 18:43:38 +0100

    implement About, Art, Menu and Contact sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
     .from(titleSplit.words, {
       opacity:0, duration:1, yPercent: 100, ease: 'expo.out', stagger: 0.02
     })
-    .from(['.top-grid', 'bottom-grid'], {
+    .from(['.top-grid', '.bottom-grid'], {
       opacity: 0, duration: 1, ease: 'power1.inOut', stagger:0.04,
     }, '-=0.5')
 
